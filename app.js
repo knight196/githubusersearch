311b3724e579296484b515e50782d866a3dbf2f2
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2021-09-14 12:03:35 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+
+function txt(){
+
+  document.getElementById('input').addEventListener('change', pop);
+
+
+function pop(){
+
+var search = document.getElementById('input').value;
+
+var originalName = search.split(' ').join('')
+
+fetch('https://api.github.com/users'+originalName)
+.then(response => response.json())
+.then((data) => {
+  console.log(data);
+  document.getElementById('result').innerHTML = `
+<img src="${data.avatar_url}"/>
+<p>${data.login}</p>
+<div class="status">
+<p>${data.followers}</p>
+<p>${data.following}</p>
+</div>
+  `
+})
+
+}
+pop();
+}
+
+
+txt();
