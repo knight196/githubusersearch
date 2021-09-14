b62ebc0d92d35dee8bb9b87782be165ed5fa6204
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2021-09-14 15:31:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
-function txt(){
+var button = document.getElementById('search');
+
+button.addEventListener('click', () => {
 
   document.getElementById('input').addEventListener('change', pop);
 
@@ -13,26 +15,30 @@ var originalName = search.split(' ').join('')
 fetch('https://api.github.com/users/'+originalName)
 .then(response => response.json())
 .then((data) => {
-document.getElementById('result').innerHTML = `
+document.getElementById('searchprofile').innerHTML = `
 <img src="${data.avatar_url}"/>
 <br>
 <p>${data.login}</p>
 <br>
 <div class="status">
-<p>Followers ${data.followers}</p>
-<p>Following ${data.following}</p>
-<p>Repos ${data.public_repos}</p>
+<p>Followers</p>
+<p>Following</p>
+<p>Repos</p>
 </div>
+
+<div class="status">
+<p>${data.followers}</p>
+<p>${data.following}</p>
+<p>${data.public_repos}</p>
+</div>
+
 <div class="status">
 <p>${data.location}</p>
 <p>${data.company}</p>
 </div>
   `
 })
-
+document.querySelector('.defaultview').style.display="none";
 }
 pop();
-}
-
-
-txt();
+});
