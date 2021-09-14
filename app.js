5b04adfe4708a6aaf5227cffec8084ea48c4fa2a
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2021-09-14 14:47:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,13 +13,19 @@ var originalName = search.split(' ').join('')
 fetch('https://api.github.com/users/'+originalName)
 .then(response => response.json())
 .then((data) => {
-
-  document.getElementById('result').innerHTML = `
+document.getElementById('result').innerHTML = `
 <img src="${data.avatar_url}"/>
+<br>
 <p>${data.login}</p>
+<br>
 <div class="status">
-<p>${data.followers}</p>
-<p>${data.following}</p>
+<p>Followers ${data.followers}</p>
+<p>Following ${data.following}</p>
+<p>Repos ${data.public_repos}</p>
+</div>
+<div class="status">
+<p>${data.location}</p>
+<p>${data.company}</p>
 </div>
   `
 })
